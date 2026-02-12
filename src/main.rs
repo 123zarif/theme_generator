1edaf539bcfbbd23162e6f83e77f8e75f28964a0
123zarif <zarifzuhayer20@gmail.com>
2026-02-12 16:03:32 +0600

    feat: enhance theme creation with user input for theme name and wallpaper file

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use kmeans_colors::get_kmeans_hamerly;
 use palette::{FromColor, Lab, Srgb};
 use serde::{Deserialize, Serialize};
 use std::fs;
+use std::path::Path;
 
 #[derive(Serialize, Deserialize)]
 struct ThemeColors {
@@ -29,8 +30,27 @@ struct Config {
 fn main() {
     let home = std::env::var("HOME").unwrap();
 
+    let config_path = format!("{}/.config/colorSchemes/themes.json", home);
+
+    let json_file = fs::read_to_string(&config_path).unwrap();
+    let mut config: Config = serde_json::from_str(&json_file).unwrap();
+
     let mut path = String::new();
+    let mut theme_name = String::new();
+    let mut file_name = String::new();
+
+    println!("Insert wallpaper path:");
     std::io::stdin().read_line(&mut path).expect("Dumb ass!");
+
+    println!("Enter Theme Name:");
+    std::io::stdin()
+        .read_line(&mut theme_name)
+        .expect("Dumb ass!");
+
+    println!("Enter Image File Name: (No Extension)");
+    std::io::stdin()
+        .read_line(&mut file_name)
+        .expect("Dumb ass!");
 
     let img = image::open(path.trim()).unwrap();
 
@@ -89,13 +109,18 @@ fn main() {
     println!("3: {:#?}", hex_3);
     println!("4: {:#?}", hex_4);
 
-    println!("Successful color extraction NIgga!");
+    println!("Successful color extraction!");
 
-    let config_path = format!("{}/.config/colorSchemes/themes.json", home);
+    let image_ext = Path::new(&path)
+        .extension()
+        .and_then(std::ffi::OsStr::to_str)
+        .unwrap_or("");
+    let image_file_name = format!("{}.{}", file_name.trim(), image_ext.trim());
 
-    let json_file = fs::read_to_string(&config_path).unwrap();
-    let mut config: Config = serde_json::from_str(&json_file).unwrap();
-    println!("{:#?}", json_file);
+    let dest_folder = format!("{}/.config/colorSchemes/wallpapers/", &home);
+    let dest_path = Path::new(&dest_folder.trim()).join(&image_file_name.trim());
+
+    fs::copy(path.trim(), &dest_path).unwrap();
 
     let colors = ThemeColors {
         primary: hex_1,
@@ -105,16 +130,16 @@ fn main() {
     };
 
     let theme = Theme {
-        name: "Test".to_string(),
+        name: theme_name.trim().to_string(),
         quickshell_theme: "Default".to_string(),
         selected: false,
         colors: colors,
-        wallpaper: "catpuccin.jpg".to_string(),
+        wallpaper: image_file_name,
     };
 
     config.themes.push(theme);
 
     let updated_config = serde_json::to_string_pretty(&config).unwrap();
 
-    fs::write(&config_path, updated_config).unwrap();
+    fs::write(&config_path, &updated_config).unwrap();
 }
